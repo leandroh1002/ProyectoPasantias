0ac8e8e0233b8deb6560b5d07d4991ab401e9197
leandroh1002 <leandroherrera1002@gmail.com>
2024-05-20 18:33:10 -0300

    margen en el boton

--- Client/src/components/ButtonDefault.jsx
+++ Client/src/components/ButtonDefault.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ButtonDefault({props}) {
   return (
-    <button class="inline-flex text-white bg-[#ca7d10] border-0 py-2 px-6 focus:outline-none hover:bg-[#ca7d10] rounded text-lg">{props}</button>
+    <button class="inline-flex text-white mx-4 bg-[#ca7d10] border-0 py-2 px-6 focus:outline-none hover:bg-[#ca7d10] rounded text-lg">{props}</button>
   )
 }
 
--- Client/src/components/Card/Card.jsx
+++ Client/src/components/Card/Card.jsx
@@ -23,6 +23,7 @@ function Card({idPublish, namePublish, carrer, enterprise, image, offer, task, o
               {/* <img alt="Tania Andrew"
             src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1480&amp;q=80"
             class="relative inline-block h-[74px] w-[74px] !rounded-full border-2 border-white object-cover object-center" /> */}
+            <ButtonDefault props="Iniciar sesion"/>
             <ButtonDefault props="Detalles"/>
         </Link>
         </div>
