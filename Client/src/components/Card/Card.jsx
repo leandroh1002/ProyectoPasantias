18964ad9946edca125dd84a9f6c5954e20b3a2b8
leandroh1002 <leandroherrera1002@gmail.com>
2024-05-20 17:53:17 -0300

    se limpia la info de la card

--- Client/src/components/Card/Card.jsx
+++ Client/src/components/Card/Card.jsx
@@ -14,14 +14,15 @@ function Card({idPublish, namePublish, carrer, enterprise, image, offer, task, o
         <div class="relative p-6 px-6 py-14 md:px-12">
             <h2 class="mb-6 block font-sans text-4xl font-medium leading-[1.5] tracking-normal text-white antialiased">
         <p>{namePublish}</p>
-            {carrer}
+            {/* {carrer} */}
             </h2>
             <h5 class="block mb-4 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-gray-400">
              {enterprise}
             </h5>
-            <Link to={`/detail/${idPublish}`}><img alt="Tania Andrew"
+            <Link to={`/detail/${idPublish}`}>
+              {/* <img alt="Tania Andrew"
             src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1480&amp;q=80"
-            class="relative inline-block h-[74px] w-[74px] !rounded-full border-2 border-white object-cover object-center" />
+            class="relative inline-block h-[74px] w-[74px] !rounded-full border-2 border-white object-cover object-center" /> */}
             <ButtonDefault props="Detalles"/>
         </Link>
         </div>
